efd4286a82aeabf891ab414a28458de47a98ccf7
Timothée Peignier <timothee.peignier@tryphon.org>
2014-03-19 10:26:50 -0700

    add a word about base instruments

--- instruments.go
+++ instruments.go
@@ -14,10 +14,22 @@
 //	  ...
 //	})
 //
+// Instruments support two types of instruments:
+// Discrete instruments return a single value, and Sample instruments a sorted array of values.
+//
+// Theses base instruments are available:
+//
+// - Rate: tracks the rate of values per seconds.
+// - Reservoir: randomly samples values.
+// - Derive: tracks the rate of values based on the delta with previous value.
+// - Gauge: tracks last value.
+// - Timer: tracks durations.
+//
 // You can create custom instruments or compose new instruments form the built-in
 // instruments as long as they implements the Sample or Discrete interfaces.
 //
-// Registry also use theses interfaces, creating a custom Reporter should be trivial, for example:
+// Registry enforce the Discrete and Sample interfaces,
+// creating a custom Reporter should be trivial, for example:
 //
 // 	for k, m := range r.Instruments() {
 // 	 	switch i := m.(type) {
