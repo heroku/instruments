8e44c0c68e79aa0e45766203509b2a53b8c8a159
Timothée Peignier <timothee.peignier@tryphon.org>
2014-03-18 17:07:36 -0700

    add overview

--- instruments.go
+++ instruments.go
@@ -1,5 +1,35 @@
-// Instruments allows you to collects metrics
-// over discrete time intervals.
+// Instruments allows you to collects metrics over discrete time intervals.
+//
+// Collected metrics will only reflect observations from last time window only,
+// rather than including observations from prior windows, contrary to EWMA based metrics.
+//
+// 	timer := instruments.NewTimer(-1)
+//
+//	registry := instruments.NewRegistry()
+//	registry.Register("processing-time", timer)
+//
+//	go reporter.Log("process", registry, time.Minute)
+//
+//	timer.Time(func() {
+//	  ...
+//	})
+//
+// You can create custom instruments or compose new instruments form the built-in
+// instruments as long as they implements the Sample or Discrete interfaces.
+//
+// Registry also use theses interfaces, creating a custom Reporter should be trivial, for example:
+//
+// 	for k, m := range r.Instruments() {
+// 	 	switch i := m.(type) {
+// 	 	case instruments.Discrete:
+// 	 	 	s := i.Snapshot()
+// 	 	 	report(k, s)
+// 	 	case instruments.Sample:
+// 	 	 	s := instruments.Quantile(i.Snapshot(), 0.95)
+// 	 	 	report(k, s)
+// 	 	}
+//	}
+//
 package instruments
 
 import (
